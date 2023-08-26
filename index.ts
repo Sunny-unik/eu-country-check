6b8105ae9dda49eceb3c6bcf8c06deabb38ebac6
sunny-unik <sunnygandhwani027@gmail.com>
2023-08-26 13:38:27 +0530

    update EU countries list

--- index.ts
+++ index.ts
@@ -1,59 +1,36 @@
-//List of CountryCodes that that cosists in european countries
+// List of CountryCodes that consists in european countries
 const EuCountries: string[] = [
-  "AX",
-  "AL",
-  "AD",
   "AT",
-  "BY",
   "BE",
-  "BA",
   "BG",
   "HR",
+  "CY",
   "CZ",
   "DK",
   "EE",
-  "FO",
   "FI",
   "FR",
   "DE",
-  "GI",
   "GR",
-  "GG",
-  "VA",
   "HU",
-  "IS",
   "IE",
-  "IM",
   "IT",
-  "JE",
   "LV",
-  "LI",
   "LT",
   "LU",
   "MT",
-  "MD",
-  "MC",
-  "ME",
   "NL",
-  "MK",
-  "NO",
   "PL",
   "PT",
   "RO",
-  "RU",
-  "SM",
-  "RS",
   "SK",
   "SI",
   "ES",
-  "SJ",
   "SE",
-  "CH",
-  "UA",
-  "GB"
 ];
 
-const isEuCountry = (countryCode: string): boolean => EuCountries.includes(countryCode);
+const isEuCountry = (countryCode: string): boolean =>
+  EuCountries.includes(countryCode);
 
 const listEuCodes = (): string[] => EuCountries;
 
